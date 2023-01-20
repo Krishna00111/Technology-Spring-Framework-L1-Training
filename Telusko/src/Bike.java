162bc1b5eafc26ae53ed6b69bdd28d80dd7248aa
Chinmay Deshmukh <42055861+Krishna00111@users.noreply.github.com>
2023-01-20 11:33:37 +0530

    Add files via upload

---
+++ Telusko/src/Bike.java
@@ -0,0 +1,11 @@
+package com.chinmay.Telusko;
+
+@Component
+public class Bike implements Vehicle {
+	
+	public void drive()
+	{
+		System.out.println("Hare Krishna...");
+	}
+
+}
---
+++ Telusko/src/Car.java
@@ -0,0 +1,11 @@
+package com.chinmay.Telusko;
+
+import org.springframework.stereotype.Component;
+public class Car implements Vehicle {
+	
+	public void drive() 
+	{
+		System.out.println("HareKrishna");
+	}
+
+}
